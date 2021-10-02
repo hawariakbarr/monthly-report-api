c0f49cc102092d6ae47a1b0c9191a05699fbb3b2
Nur Imam Hawari Akbar <imamhawari19@gmail.com>
2021-10-02 17:22:07 +0700

    little feedback for deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort, jsonify, make_response,  render_template, redirect, url_for
 import jwt
 import pandas as pd
-import mysql.connector
+# import mysql.connector
 
 import os, datetime
 from os.path import join, dirname, realpath
@@ -48,88 +48,88 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://%(user)s:%(pass)s@%(host)s:%(port)s/%(db)s'% MYSQL
 
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="kurakuraninja14",
-  database="monthly_report"
-)
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password="kurakuraninja14",
+#   database="monthly_report"
+# )
 
 db.init_app(app)
 
 
-mycursor = mydb.cursor()
+# mycursor = mydb.cursor()
 
 # Root URL
-@app.route('/')
-def index():
+# @app.route('/')
+# def index():
      # Set The upload HTML template '\templates\index.html'
-    return render_template('upload.html')
+    # return render_template('upload.html')
 
 
-# Get the uploaded files
-@app.route("/", methods=['POST'])
-def uploadFiles():
-      # get the uploaded file
-    try:
-        uploaded_file = request.files['file']
-        if uploaded_file.filename != '':
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-            # set the file path
-            uploaded_file.save(file_path)
-            # save the file
-            saveData(file_path)
-    except Exception as e:
-        print(e)    
+# # Get the uploaded files
+# @app.route("/", methods=['POST'])
+# def uploadFiles():
+#       # get the uploaded file
+#     try:
+#         uploaded_file = request.files['file']
+#         if uploaded_file.filename != '':
+#             file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+#             # set the file path
+#             uploaded_file.save(file_path)
+#             # save the file
+#             saveData(file_path)
+#     except Exception as e:
+#         print(e)    
 
-    return redirect(url_for('index'))
+#     return redirect(url_for('index'))
 
-def saveData(filePath):
-    if filePath.find("OPD") != -1:
-        col_names = ['name','address', 'pic', 'phone_number']
-        csvData = pd.read_csv(filePath, names=col_names, skiprows = 1, keep_default_na=False)
+# def saveData(filePath):
+#     if filePath.find("OPD") != -1:
+#         col_names = ['name','address', 'pic', 'phone_number']
+#         csvData = pd.read_csv(filePath, names=col_names, skiprows = 1, keep_default_na=False)
     
-        for i,row in csvData.iterrows():
-            print(i, row['name'],row['address'],row['pic'],row['phone_number'], datetime.datetime.now())
-            sql = "INSERT INTO master_opd (name, address, pic, phone_number, created_at) VALUES (%s, %s, %s, %s, %s)"
-            value = (row['name'].title(),row['address'].title(),row['pic'].title(),row['phone_number'], datetime.datetime.now())
-            mycursor.execute(sql, value)
-            mydb.commit()
+#         for i,row in csvData.iterrows():
+#             print(i, row['name'],row['address'],row['pic'],row['phone_number'], datetime.datetime.now())
+#             sql = "INSERT INTO master_opd (name, address, pic, phone_number, created_at) VALUES (%s, %s, %s, %s, %s)"
+#             value = (row['name'].title(),row['address'].title(),row['pic'].title(),row['phone_number'], datetime.datetime.now())
+#             mycursor.execute(sql, value)
+#             mydb.commit()
 
-    elif filePath.find("UPTD") != -1:
-        col_names = ['opd_id','name','address', 'pic', 'phone_number']
-        csvData = pd.read_csv(filePath, names=col_names, skiprows = 1, keep_default_na=False)
+#     elif filePath.find("UPTD") != -1:
+#         col_names = ['opd_id','name','address', 'pic', 'phone_number']
+#         csvData = pd.read_csv(filePath, names=col_names, skiprows = 1, keep_default_na=False)
     
-        for i,row in csvData.iterrows():
-            print(i, row['opd_id'], row['name'],row['address'],row['pic'],row['phone_number'], datetime.datetime.now())
-            sql = "INSERT INTO master_uptd (opd_id, name, address, pic, phone_number, created_at) VALUES (%s, %s, %s, %s, %s, %s)"
-            value = (int(row['opd_id']), row['name'].title(), row['address'].title(), row['pic'].title(), row['phone_number'], datetime.datetime.now())
-            mycursor.execute(sql, value)
-            mydb.commit()
+#         for i,row in csvData.iterrows():
+#             print(i, row['opd_id'], row['name'],row['address'],row['pic'],row['phone_number'], datetime.datetime.now())
+#             sql = "INSERT INTO master_uptd (opd_id, name, address, pic, phone_number, created_at) VALUES (%s, %s, %s, %s, %s, %s)"
+#             value = (int(row['opd_id']), row['name'].title(), row['address'].title(), row['pic'].title(), row['phone_number'], datetime.datetime.now())
+#             mycursor.execute(sql, value)
+#             mydb.commit()
 
-    elif filePath.find("opd link") != -1:
-        col_names = ['prtg_id','opd_id','isp_id', 'band_id']
-        csvData = pd.read_csv(filePath, names=col_names, skiprows = 1)
-        csvData = (csvData.fillna(0)).astype(int)
-        dtypes = {'prtg_id': 'int', 'opd_id': 'int', 'isp_id': 'int', 'band_id': 'int'}
-        for i,row in csvData.iterrows():
-            print(i, row['prtg_id'], row['opd_id'],row['isp_id'],row['band_id'], datetime.datetime.now())
-            sql = "INSERT INTO opd_link (prtg_id, opd_id, isp_id, band_id,  created_at) VALUES (%s, %s, %s, %s, %s)"
-            value = (int(row['prtg_id']), int(row['opd_id']), int(row['isp_id']), int(row['band_id']), datetime.datetime.now())
-            mycursor.execute(sql, value)
-            mydb.commit()
+#     elif filePath.find("opd link") != -1:
+#         col_names = ['prtg_id','opd_id','isp_id', 'band_id']
+#         csvData = pd.read_csv(filePath, names=col_names, skiprows = 1)
+#         csvData = (csvData.fillna(0)).astype(int)
+#         dtypes = {'prtg_id': 'int', 'opd_id': 'int', 'isp_id': 'int', 'band_id': 'int'}
+#         for i,row in csvData.iterrows():
+#             print(i, row['prtg_id'], row['opd_id'],row['isp_id'],row['band_id'], datetime.datetime.now())
+#             sql = "INSERT INTO opd_link (prtg_id, opd_id, isp_id, band_id,  created_at) VALUES (%s, %s, %s, %s, %s)"
+#             value = (int(row['prtg_id']), int(row['opd_id']), int(row['isp_id']), int(row['band_id']), datetime.datetime.now())
+#             mycursor.execute(sql, value)
+#             mydb.commit()
     
-    elif filePath.find("uptd link") != -1:
-        col_names = ['prtg_id','uptd_id','isp_id', 'band_id']
-        csvData = pd.read_csv(filePath, names=col_names, skiprows = 1)
-        csvData = (csvData.fillna(0)).astype(int)
-        dtypes = {'prtg_id': 'int', 'uptd_id': 'int', 'isp_id': 'int', 'band_id': 'int'}
-        for i,row in csvData.iterrows():
-            print(i, row['prtg_id'], row['uptd_id'],row['isp_id'],row['band_id'], datetime.datetime.now())
-            sql = "INSERT INTO uptd_link (prtg_id, uptd_id, isp_id, band_id,  created_at) VALUES (%s, %s, %s, %s, %s)"
-            value = (int(row['prtg_id']), int(row['uptd_id']), int(row['isp_id']), int(row['band_id']), datetime.datetime.now())
-            mycursor.execute(sql, value)
-            mydb.commit()
+#     elif filePath.find("uptd link") != -1:
+#         col_names = ['prtg_id','uptd_id','isp_id', 'band_id']
+#         csvData = pd.read_csv(filePath, names=col_names, skiprows = 1)
+#         csvData = (csvData.fillna(0)).astype(int)
+#         dtypes = {'prtg_id': 'int', 'uptd_id': 'int', 'isp_id': 'int', 'band_id': 'int'}
+#         for i,row in csvData.iterrows():
+#             print(i, row['prtg_id'], row['uptd_id'],row['isp_id'],row['band_id'], datetime.datetime.now())
+#             sql = "INSERT INTO uptd_link (prtg_id, uptd_id, isp_id, band_id,  created_at) VALUES (%s, %s, %s, %s, %s)"
+#             value = (int(row['prtg_id']), int(row['uptd_id']), int(row['isp_id']), int(row['band_id']), datetime.datetime.now())
+#             mycursor.execute(sql, value)
+#             mydb.commit()
 
 @app.route('/addition/<int:firstNumber>/<int:secondNumber>')
 def addition(firstNumber,secondNumber):
